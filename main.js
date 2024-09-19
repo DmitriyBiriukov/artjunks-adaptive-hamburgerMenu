c92d0a07f4817e3c1aee82004964077cdf4fc852
Dmytro Biriukov <dmitriybirukoff@gmail.com>
2024-09-19 12:03:59 +0300

    fixed bug with menu

--- main.js
+++ main.js
@@ -5,39 +5,27 @@ let body = document.getElementById("body");
 // Начальное состояние меню
 menuList.style.maxHeight = "0px";
 
+// Переменная для отслеживания состояния меню
+let isMenuOpen = false;
+
 function toggleMenu() {
-  // Получаем ширину экрана
-  const screenWidth = window.innerWidth;
-
-  if (screenWidth > 1054) {
-    // Если ширина экрана больше 1054px, устанавливаем стили для закрытого меню
-    menuList.style.maxHeight = "0px";
-    menuList.style.backgroundColor = "none";
-    menuList.style.paddingTop = "0px";
-    menuList.style.paddingBottom = "0px";
-    body.style.overflow = "auto"; // Разрешаем прокрутку
-    hambMenu.src = "/img/Hamburger Menu.svg"; // Меняем изображение обратно на бургер
-    return; // Выходим из функции
-  }
-
-  // Открываем меню если ширина экрана меньше или равна 1054px
-  if (menuList.style.maxHeight === "0px" || menuList.style.maxHeight === "") {
+  if (!isMenuOpen) {
+    // Открываем меню
     menuList.style.maxHeight = "100%";
     menuList.style.backgroundColor = "black";
     menuList.style.paddingTop = "100px";
     menuList.style.paddingBottom = "100px";
-    body.style.overflow = "hidden";
+    body.style.overflow = "hidden"; // Отключаем прокрутку страницы
     hambMenu.src = "/img/back.svg"; // Меняем изображение на крестик
+    isMenuOpen = true; // Меняем состояние на открытое меню
   } else {
     // Закрываем меню
     menuList.style.maxHeight = "0px";
     menuList.style.backgroundColor = "none";
     menuList.style.paddingTop = "0px";
     menuList.style.paddingBottom = "0px";
-    body.style.overflow = "auto";
+    body.style.overflow = "auto"; // Включаем прокрутку страницы
     hambMenu.src = "/img/Hamburger Menu.svg"; // Меняем изображение обратно на бургер
+    isMenuOpen = false; // Меняем состояние на закрытое меню
   }
 }
-
-// Добавляем обработчик события изменения размера окна
-window.addEventListener("resize", toggleMenu);
